1bd1d2b83afbd9bdbf9417c2249cdf4257eefe8f
Jacob Beard <jake@minnow.io>
2018-03-24 18:24:41 -0400

    Initial implementation of login on AWS cognito.

--- src/app/App.js
+++ src/app/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
+import Amplify from 'aws-amplify';
+import { withAuthenticator } from 'aws-amplify-react';
+import aws_exports from './aws-exports';
+
 import Map from "./map/Map";
 import Login from "./login/Login";
 import Form from "./form/Form";
 
 import "./App.css";
+
+Amplify.configure(aws_exports);
 
 class App extends Component {
   render() {
@@ -23,4 +29,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default withAuthenticator(App);
---
+++ src/app/aws-exports.js
@@ -0,0 +1,10 @@
+export default {
+  Auth: {
+  // REQUIRED - Amazon Cognito Identity Pool ID
+      identityPoolId: 'us-east-2:a474fd65-5d53-4a95-8c88-62b800b8fbaf', 
+  // REQUIRED - Amazon Cognito Region
+      region: 'us-east-2',
+      userPoolId: 'us-east-2_50o2ELF35',
+      userPoolWebClientId: '6biku4hn0kfpe52c50tdq6opd0'
+  }
+}
