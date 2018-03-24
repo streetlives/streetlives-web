a15a8be6ceec9ceebdb2313c3be7c3ae03530832
nlaz <nikolazaris@utexas.edu>
2018-03-24 14:54:56 -0400

    Add button props and add to storybook

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,21 @@
 import React, { Component } from "react";
+import cx from "classnames";
+import "./Button.css";
 
 class Button extends Component {
   render() {
-    const { children } = this.props;
-    return <button>{children}</button>;
+    const { onClick, children, primary, secondary, fluid } = this.props;
+    const classNames = cx("Button", {
+      "Button-primary": primary,
+      "Button-secondary": secondary,
+      "Button-fluid": fluid,
+    });
+
+    return (
+      <button onClick={onClick} className={classNames}>
+        {children}
+      </button>
+    );
   }
 }
 
