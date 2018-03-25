7d8d2b217e241af2f5a22d5b6f613f14150c9308
nlaz <nikolazaris@utexas.edu>
2018-03-25 18:01:56 -0400

    Add input component

---
+++ src/components/input/Input.story.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import { storiesOf } from "@storybook/react";
+
+import Input from "./Input";
+
+const DefaultExample = (
+  <div className="container-fluid">
+    <label className="w-100 mt-4">Default Input</label>
+    <Input placeholder="Enter your email..." />
+  </div>
+);
+
+const FluidExample = (
+  <div className="container-fluid">
+    <label className="w-100 mt-4">Full Width Input</label>
+    <Input fluid placeholder="Enter your email..." />
+  </div>
+);
+
+storiesOf("Input", module)
+  .add("Overview", () => [DefaultExample, FluidExample])
+  .add("Default example", () => DefaultExample);
