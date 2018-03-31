b4fafa82a0f3098661451bdee8745dffa2cc7615
Jacob Beard <jake@minnow.io>
2018-03-31 10:20:12 -0400

    Added a Google Map.

--- src/app/App.js
+++ src/app/App.js
@@ -17,18 +17,6 @@ class App extends Component {
   componentWillMount(){
     Amplify.Auth.currentAuthenticatedUser().then((user) => {
       console.log('user',user)
-      const apigClient = window.apigClientFactory.newClient();
-      const idJwtToken = user.signInUserSession.getIdToken().getJwtToken();
-      apigClient.locationsGet(
-        {},
-        {},
-        {
-          headers : {
-            Authorization : idJwtToken 
-          }
-        })
-        .then((result) => console.log('result', result))
-        .catch( (e) => console.log('error', e) )
       user.getUserAttributes((err, attributes) => {
         let o = {}; 
         attributes.forEach(x => o[x.Name] = x.Value)
