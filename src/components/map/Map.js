45a5e0e3809c1b6dd5fd4773392cc795f4679e8b
rovack <rovack@walla.co.il>
2018-04-09 00:16:33 -0400

    Move URLs to constants file

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -2,16 +2,11 @@ import React from "react";
 import { compose, withProps, withStateHandlers, lifecycle } from "recompose";
 import { withScriptjs, withGoogleMap, GoogleMap } from "react-google-maps";
 import LocationMarker from './LocationMarker';
+import urls from "../../constants/urls";
 
 const Map = compose(
   withProps({
-    /**
-     * Note: create and replace your own key in the Google console.
-     * https://console.developers.google.com/apis/dashboard
-     * The key "AIzaSyBkNaAGLEVq0YLQMi-PYEMabFeREadYe1Q" can be ONLY used in this sandbox (no forked).
-     */
-    googleMapURL:
-      "https://maps.googleapis.com/maps/api/js?key=AIzaSyCFMDqEgQ6VWWJbROzZhHu-f6sktAmTEGU&v=3.exp&libraries=geometry,drawing,places",
+    googleMapURL: urls.googleMaps,
     loadingElement: <div style={{ height: `100%` }} />,
     containerElement: <div style={{ height: `400px` }} />,
     mapElement: <div style={{ height: `100%` }} />
@@ -23,7 +18,6 @@ const Map = compose(
       openLocationId: openLocationId === toggledLocationId ? null : toggledLocationId,
     })
   }),
-  // TODO: This is disgusting. I'm pretty amazed this is actually in the official examples.
   lifecycle({
     componentWillMount() {
       let mapRef;
--- src/services/api.js
+++ src/services/api.js
@@ -1,14 +1,13 @@
 import axios from "axios";
 import Amplify from 'aws-amplify';
-
-const baseUrl = 'https://w6pkliozjh.execute-api.us-east-1.amazonaws.com/prod';
+import urls from "../constants/urls";
 
 export const getLocations = ({ latitude, longitude, radius, searchString }) =>
   Amplify.Auth.currentAuthenticatedUser().then((user) => {
     const idJwtToken = user.signInUserSession.getIdToken().getJwtToken();
 
     return axios.request({
-        url : `${baseUrl}/locations`,
+        url : `${urls.baseApi}/locations`,
         method : 'get',
         params: { latitude, longitude, radius, searchString },
         headers : {
