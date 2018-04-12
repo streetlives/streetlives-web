c9bae75a7906426c3679f9d2def90646af913d74
nlaz <nikolazaris@utexas.edu>
2018-04-12 17:44:35 -0400

    Update button with prop types

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import cx from 'classnames';
+import PropTypes from 'prop-types';
+
 import './Button.css';
 
-function Button() {
-  const {
-    onClick, children, basic, primary, secondary, disabled, fluid,
-  } = this.props;
+function Button({
+  onClick, children, basic, primary, secondary, disabled, fluid,
+}) {
   const classNames = cx('Button', {
     'Button-primary': primary,
     'Button-secondary': secondary,
@@ -20,5 +21,23 @@ function Button() {
     </button>
   );
 }
+
+Button.defaultProps = {
+  basic: false,
+  primary: false,
+  secondary: false,
+  disabled: false,
+  fluid: false,
+};
+
+Button.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  children: PropTypes.element.isRequired,
+  basic: PropTypes.bool,
+  primary: PropTypes.bool,
+  secondary: PropTypes.bool,
+  disabled: PropTypes.bool,
+  fluid: PropTypes.bool,
+};
 
 export default Button;
