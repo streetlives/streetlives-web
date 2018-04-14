651a6aa11a3aa11de5b0d484cff613ac2e951b89
nlaz <nikolazaris@utexas.edu>
2018-04-14 19:05:34 -0400

    Update location form with different questions

--- src/app/locationForm/LocationForm.jsx
+++ src/app/locationForm/LocationForm.jsx
@@ -4,16 +4,74 @@ import ProgressBar from '../locationInfo/ProgressBar';
 import Header from '../../components/header';
 import Button from '../../components/button';
 import Icon from '../../components/icon';
+import Input from '../../components/input';
 
-function FormQuestion() {
-  return (
-    <div>
-      <Header>Please take a picture of this location&apos;s main street entrance</Header>
-      <Button onClick={() => {}} primary className="mt-3">
-        <Icon name="camera" size="lg" />
-      </Button>
-    </div>
-  );
+function FormQuestion({ index }) {
+  switch (index) {
+    case 0:
+      return (
+        <div>
+          <Header>Please take a picture of this location&apos;s main street entrance</Header>
+          <Button onClick={() => {}} primary className="mt-3">
+            <Icon name="camera" size="lg" />
+          </Button>
+        </div>
+      );
+    case 1:
+      return (
+        <div>
+          <Header>What&apos;s this organization&apos;s name?</Header>
+          <Input fluid placeholder="Enter the name of the organization" />
+          <Button onClick={() => {}} primary className="mt-3">
+            OK
+          </Button>
+        </div>
+      );
+    case 2:
+      return (
+        <div>
+          <Header>What&apos;s this organization&apos;s phone number?</Header>
+          <Input fluid placeholder="e.g. 646-909-4591" />
+          <Input fluid placeholder="Extension (if any)" />
+          <Button onClick={() => {}} primary className="mt-3">
+            OK
+          </Button>
+        </div>
+      );
+    case 3:
+      return (
+        <div>
+          <Header>What&apos;s this organization&apos;s address?</Header>
+          <Input fluid placeholder="Enter address" />
+          <Button onClick={() => {}} primary className="mt-3">
+            OK
+          </Button>
+        </div>
+      );
+    case 4:
+      return (
+        <div>
+          <Header>What&apos;s this organization&apos;s website?</Header>
+          <Input fluid placeholder="Enter website" />
+          <Button onClick={() => {}} primary className="mt-3">
+            OK
+          </Button>
+        </div>
+      );
+    case 5:
+      return (
+        <div>
+          <Header>What&apos;s this location&apos;s name?</Header>
+          <Input fluid placeholder="Enter the name of the organization" />
+          <Button onClick={() => {}} primary className="mt-3">
+            OK
+          </Button>
+        </div>
+      );
+
+    default:
+      return <div>Form Question</div>;
+  }
 }
 
 class LocationForm extends Component {
@@ -42,7 +100,7 @@ class LocationForm extends Component {
         <ProgressBar step={index} />
         <div className="container">
           <div className="row px-4">
-            <FormQuestion />
+            <FormQuestion index={index} />
           </div>
         </div>
         <div className="position-absolute" style={{ right: 0, bottom: 12 }}>
