71a3b71e4fc6e565ff305546f97800ffda1c6e0b
nlaz <nikolazaris@utexas.edu>
2018-04-15 14:21:12 -0400

    Update environment variables

--- src/config.js
+++ src/config.js
@@ -1,3 +1,6 @@
+/* eslint-disable max-len */
 export default {
   gaTrackingId: process.env.REACT_APP_GA_TRACKING_ID || 'UA-116475051-1',
+  baseApi: process.env.REACT_APP_API_URL || 'http://localhost:3001',
+  googleMaps: process.env.REACT_APP_GMAPS_URL,
 };
--- src/services/api.js
+++ src/services/api.js
@@ -1,33 +1,41 @@
-import axios from "axios";
+import axios from 'axios';
 import Amplify from 'aws-amplify';
-import urls from "../constants/urls";
+import config from '../config';
 
-export const getLocations = ({ latitude, longitude, radius, searchString }) =>
+export const getLocations = ({
+  latitude, longitude, radius, searchString,
+}) =>
   Amplify.Auth.currentAuthenticatedUser().then((user) => {
     const idJwtToken = user.signInUserSession.getIdToken().getJwtToken();
 
-    return axios.request({
-        url : `${urls.baseApi}/locations`,
-        method : 'get',
-        params: { latitude, longitude, radius, searchString },
-        headers : {
-          Authorization : idJwtToken
-        }
+    return axios
+      .request({
+        url: `${config.baseApi}/locations`,
+        method: 'get',
+        params: {
+          latitude,
+          longitude,
+          radius,
+          searchString,
+        },
+        headers: {
+          Authorization: idJwtToken,
+        },
       })
-      .then((result) => result.data);
+      .then(result => result.data);
   });
-
 
 export const getLocation = ({ id }) =>
   Amplify.Auth.currentAuthenticatedUser().then((user) => {
     const idJwtToken = user.signInUserSession.getIdToken().getJwtToken();
 
-    return axios.request({
-        url : `${urls.baseApi}/locations/${id}`,
-        method : 'get',
-        headers : {
-          Authorization : idJwtToken
-        }
+    return axios
+      .request({
+        url: `${config.baseApi}/locations/${id}`,
+        method: 'get',
+        headers: {
+          Authorization: idJwtToken,
+        },
       })
-      .then((result) => result.data);
+      .then(result => result.data);
   });
