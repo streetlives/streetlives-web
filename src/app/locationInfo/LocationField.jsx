c096fa274a142085797d55ddeb5423d1e679fbdc
nlaz <nikolazaris@utexas.edu>
2018-04-19 19:27:49 -0400

    create helper methods for field types

--- src/app/locationInfo/LocationField.jsx
+++ src/app/locationInfo/LocationField.jsx
@@ -6,6 +6,7 @@ import { withRouter } from 'react-router-dom';
 
 import Badge from '../../components/badge';
 import Icon from '../../components/icon';
+import { FIELD_TYPES, getFieldType } from './utils';
 
 class LocationField extends Component {
   constructor() {
@@ -20,19 +21,24 @@ class LocationField extends Component {
   render() {
     const { title, updatedAt } = this.props;
 
+    const fieldType = getFieldType(updatedAt);
     const classNames = {
       field: cx('LocationField border-top border-bottom'),
       emptyDot: cx('rounded-circle mr-2', {
-        'bg-danger': !updatedAt || moment(updatedAt).isBefore(moment().subtract(1, 'years')),
-        'bg-warning': updatedAt && moment(updatedAt).isBefore(moment().subtract(6, 'months')),
-        'bg-success': updatedAt && moment(updatedAt).isBefore(moment()),
+        'bg-success': fieldType === FIELD_TYPES.SUCCESS,
+        'bg-warning': fieldType === FIELD_TYPES.WARNING,
+        'bg-danger': fieldType === FIELD_TYPES.DANGER || fieldType === FIELD_TYPES.MISSING,
       }),
       updatedAt: cx('font-weight-light', {
-        'text-uppercase text-danger font-weight-bold': !updatedAt,
+        'text-uppercase text-danger font-weight-bold': fieldType === FIELD_TYPES.MISSING,
       }),
     };
 
-    const updatedAtText = updatedAt ? moment(updatedAt).fromNow() : 'Missing info. Please add!';
+    const shouldVerify = fieldType === FIELD_TYPES.DANGER || fieldType === FIELD_TYPES.WARNING;
+    const isMissing = fieldType === FIELD_TYPES.MISSING;
+
+    const timeAgoText = !isMissing ? moment(updatedAt).fromNow() : 'Missing info. Please add!';
+    const updatedAtText = shouldVerify ? `${timeAgoText}. Please verify.` : timeAgoText;
 
     return (
       <div className={classNames.field} onClick={this.navigateToFieldForm}>
@@ -43,12 +49,7 @@ class LocationField extends Component {
               <div className="d-flex">
                 <Badge>
                   <div className={classNames.emptyDot} style={{ height: '10px', width: '10px' }} />
-                  <small className={classNames.updatedAt}>
-                    {updatedAtText}
-                    {moment(updatedAt).isBefore(moment().subtract(6, 'months'))
-                      ? '. Please verify.'
-                      : ''}
-                  </small>
+                  <small className={classNames.updatedAt}>{updatedAtText}</small>
                 </Badge>
               </div>
             </div>
