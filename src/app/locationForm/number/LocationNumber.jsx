fdbc3ab9591e05a27573c137d8ba06928a680128
Jacob Beard <jake@minnow.io>
2018-04-20 01:19:59 -0400

    Updated phone number form to use Form HOC

--- src/app/locationForm/number/LocationNumber.jsx
+++ src/app/locationForm/number/LocationNumber.jsx
@@ -1,19 +1,52 @@
 import React from 'react';
-import Header from '../../../components/header';
-import Button from '../../../components/button';
-import Input from '../../../components/input';
+import { connect } from 'react-redux';
+import { selectLocationData } from '../../../reducers';
+import { updateLocation } from '../../../actions';
+import { getLocation } from '../../../actions';
+import Form from '../common/Form';
+import FormEdit from '../common/FormEdit';
+import FormView from '../common/FormView';
+import { compose, withProps } from 'recompose';
 
-function LocationNumber(props) {
-  return (
-    <div>
-      <Header>What&apos;s this organization&apos;s phone number?</Header>
-      <Input fluid placeholder="e.g. 646-909-4591" />
-      <Input fluid placeholder="Extension (if any)" />
-      <Button onClick={() => {}} primary className="mt-3">
-        OK
-      </Button>
-    </div>
-  );
-}
+const LocationNumberEdit = compose(
+  withProps({
+    headerText : 'What\'s this location\'s phone number?',
+    placeholderText : 'e.g. 646-909-4591',
+  })
+)(props => <FormEdit {...props} />)
 
-export default LocationNumber;
+const LocationNumberView = compose(
+  withProps({
+    topText : 'LOCATION PHONE NUMBER',
+  })
+)(props => <FormView {...props} />)
+
+const LocationNumber = compose(
+  withProps({
+    viewComponent: LocationNumberView,
+    editComponent: LocationNumberEdit
+  })
+)(props => <Form {...props} />)
+
+const mapStateToProps = (state, ownProps) => {
+  const locationId = ownProps.match.params.locationId;
+  const locationData = selectLocationData(state, locationId);
+
+  return {
+    value: locationData && 
+              locationData.Phones &&
+              locationData.Phones[0] && 
+              locationData.Phones[0].number ? 
+              locationData.Phones[0].number : null,
+    locationData 
+  };
+};
+
+const mapDispatchToProps = (dispatch, ownProps) => ({
+  updateValue: newLocationNumber => dispatch(updateLocation(ownProps.match.params.locationId, { Phones : [{number : newLocationNumber}]})),
+  getLocation: (locationId) => {
+    dispatch(getLocation(locationId));
+  },
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(LocationNumber);
