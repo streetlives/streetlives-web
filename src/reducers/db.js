d413d0fae7103d5dd5129cc348781c7729cec938
Jacob Beard <jake@minnow.io>
2018-04-21 20:35:49 -0700

    Move out dbReducer into its own modules.

---
+++ src/reducers/db.js
@@ -0,0 +1,36 @@
+const dbReducer = (state = {}, action) => {
+  switch(action.type){
+    case 'GET_LOCATION_RESPONSE':
+      if(action.payload){
+        return {...state, [action.payload.id] : action.payload}
+      }
+      break;
+    case 'OPTIMISTIC_UPDATE_LOCATION':
+      if(action.payload){
+        return {
+          ...state,
+          [`last/${action.payload.id}`] : state[action.payload.id],
+          [action.payload.id] : { 
+            ...state[action.payload.id],
+            ...action.payload.params,
+          }
+        }
+      }
+      break;
+    case 'ROLLBACK_UPDATE_LOCATION':
+      if(action.payload){
+        return {
+          ...state,
+          [`last/${action.payload.id}`] : null,
+          [action.payload.id] : state[`last/${action.payload.id}`]
+        }
+      }
+      break;
+    default:
+      return state;
+  }
+
+  return state;
+}
+
+export default dbReducer;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,37 +1,4 @@
-const dbReducer = (state = {}, action) => {
-  switch(action.type){
-    case 'GET_LOCATION_RESPONSE':
-      if(action.payload){
-        return {...state, [action.payload.id] : action.payload}
-      }
-      break;
-    case 'OPTIMISTIC_UPDATE_LOCATION':
-      if(action.payload){
-        return {
-          ...state,
-          [`last/${action.payload.id}`] : state[action.payload.id],
-          [action.payload.id] : { 
-            ...state[action.payload.id],
-            ...action.payload.params,
-          }
-        }
-      }
-      break;
-    case 'ROLLBACK_UPDATE_LOCATION':
-      if(action.payload){
-        return {
-          ...state,
-          [`last/${action.payload.id}`] : null,
-          [action.payload.id] : state[`last/${action.payload.id}`]
-        }
-      }
-      break;
-    default:
-      return state;
-  }
-
-  return state;
-}
+import dbReducer from './db';
 
 export const selectLocationData = (state, locationId) => state.db[locationId];
 
