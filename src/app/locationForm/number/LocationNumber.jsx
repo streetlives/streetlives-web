4f35973890e7bf9aa0ae25e9ef92ee238531ba76
Jacob Beard <jake@minnow.io>
2018-04-22 13:15:25 -0700

    Remove componentWillReceiveProps from Phone Number form, as we no longer need it, because phone number form will not be rendered until data is loaded.

--- src/app/locationForm/number/LocationNumber.jsx
+++ src/app/locationForm/number/LocationNumber.jsx
@@ -34,24 +34,6 @@ class LocationNumberEdit extends Component {
     });
   }
 
-  componentWillReceiveProps(props){
-    let o = {};
-    if(props.value.number !== this.props.value.number){
-      const [areaCode, firstThree, lastFour] = props.value.number ? props.value.number.split('.') : [];
-      o = {
-        areaCode, 
-        firstThree, 
-        lastFour
-      }
-    }
-
-    if(props.value.extension !== this.props.value.extension){
-      o.extension = props.value.extension;
-    }
-
-    if(Object.keys(o).length) this.setState(o);
-  }
-
   onChange(key, event) {
     let value = event.target.value;
     const maxLength = this.keyToMaxDigits[key];
@@ -71,43 +53,39 @@ class LocationNumberEdit extends Component {
     return (
       <form ref={ e => this.form = e } className="container" onSubmit={this.onSubmit}>
         <Header>What's this location's phone number?</Header>
-        <div>
-          (<Input
-            customValidationMessage="Area code must contain three digits"
-            type="tel"
-            value={this.state.areaCode}
-            onChange={this.onChange_areaCode}
-            pattern="\d{3}"
-            size="3"
-            required
-            />)&nbsp;-&nbsp;
-          <Input
-            customValidationMessage="Enter first three digits for phone number"
-            type="tel"
-            value={this.state.firstThree}
-            onChange={this.onChange_firstThree}
-            size="3"
-            pattern="\d{3}"
-            required
-            />&nbsp;-&nbsp;
-          <Input
-            customValidationMessage="Enter last four digits for phone number"
-            type="tel"
-            value={this.state.lastFour}
-            onChange={this.onChange_lastFour}
-            pattern="\d{4}"
-            required
-            size="4"
-            />
-        </div>
-        <div>
-          ext.
-          <Input
-            type="tel"
-            value={this.state.extension}
-            onChange={this.onChange_extension}
-            />
-        </div>
+        (<Input
+          customValidationMessage="Area code must contain three digits"
+          type="tel"
+          value={this.state.areaCode}
+          onChange={this.onChange_areaCode}
+          pattern="\d{3}"
+          size="3"
+          required
+          />)&nbsp;-&nbsp;
+        <Input
+          customValidationMessage="Enter first three digits for phone number"
+          type="tel"
+          value={this.state.firstThree}
+          onChange={this.onChange_firstThree}
+          size="3"
+          pattern="\d{3}"
+          required
+          />&nbsp;-&nbsp;
+        <Input
+          customValidationMessage="Enter last four digits for phone number"
+          type="tel"
+          value={this.state.lastFour}
+          onChange={this.onChange_lastFour}
+          pattern="\d{4}"
+          required
+          size="4"
+          />&nbsp;ext.&nbsp;
+        <Input
+          type="tel"
+          size="4"
+          value={this.state.extension}
+          onChange={this.onChange_extension}
+          />
         <input type="submit" className="Button Button-primary mt-3" value="OK"/>
       </form>
     );
