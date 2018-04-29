7a4a7fc31bebdfa360e6c8339a05adba51a0b9e6
nlaz <nikolazaris@utexas.edu>
2018-04-29 14:00:00 -0400

    add services container, add services info and recap pages

--- src/app/services/ServiceContainer.jsx
+++ src/app/services/ServiceContainer.jsx
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
-import Header from '../../components/header';
 import Button from '../../components/button';
-import Accordion from '../../components/accordion';
 import NavBar from '../NavBar';
-import Selector from '../../components/selector';
+import ServicesInfoPage from './ServicesInfoPage';
+import ServicesRecapPage from './ServicesRecapPage';
 
 const data = {
   shelter: {
@@ -20,96 +19,35 @@ const data = {
   },
 };
 
-// eslint-disable-next-line
+const PAGES = {
+  INFO: 'Services info',
+  RECAP: 'Services recap',
+};
+
 class ServicesContainer extends Component {
-  state = { active: -1, services: data };
+  state = { page: PAGES.INFO, services: data };
 
-  onToggleOpen = value =>
-    this.setState(({ active }) => ({ active: active !== value ? value : -1 }));
+  onNext = () => {
+    if (this.state.page === PAGES.INFO) {
+      this.setState({ page: PAGES.RECAP });
+    }
+  };
 
-  onToggleService = (service, id) => {
+  onSelectService = (service, id) => {
     const { services } = this.state;
     services[service][id].selected = !services[service][id].selected;
     this.setState({ services });
   };
 
-  onNext = () => {
-    console.log('onNext');
-  };
-
   render() {
-    const { active, services } = this.state;
+    const { page, services } = this.state;
     return (
       <div className="text-left">
-        <NavBar title="Services Info" />
-        <div className="py-5 px-3 container">
-          <Header>What programs and services are available at this location?</Header>
-        </div>
-        <Accordion>
-          <Accordion.Item
-            active={active === 0}
-            onClick={() => this.onToggleOpen(0)}
-            title="Shelter"
-            icon="home"
-          />
-          <Accordion.Content active={active === 0}>
-            <Selector fluid>
-              {Object.keys(services.shelter).map(id => (
-                <Selector.Option
-                  key={id}
-                  onClick={() => this.onToggleService('shelter', id)}
-                  active={services.shelter[id].selected}
-                >
-                  {services.shelter[id].name}
-                </Selector.Option>
-              ))}
-              <Selector.Option align="center">+ Add another shelter service</Selector.Option>
-            </Selector>
-          </Accordion.Content>
-
-          <Accordion.Item
-            active={active === 1}
-            onClick={() => this.onToggleOpen(1)}
-            title="Food"
-            icon="cutlery"
-          />
-          <Accordion.Content active={active === 1}>
-            <Selector fluid>
-              {Object.keys(services.food).map(id => (
-                <Selector.Option
-                  key={id}
-                  onClick={() => this.onToggleService('food', id)}
-                  active={services.food[id].selected}
-                >
-                  {services.food[id].name}
-                </Selector.Option>
-              ))}
-              <Selector.Option align="center">+ Add another food service</Selector.Option>
-            </Selector>
-          </Accordion.Content>
-
-          <Accordion.Item
-            active={active === 2}
-            onClick={() => this.onToggleOpen(2)}
-            title="Other Services"
-            icon="ellipsis-h"
-          />
-          <Accordion.Content active={active === 2}>
-            <Selector fluid>
-              {Object.keys(services.other).map(id => (
-                <Selector.Option
-                  key={id}
-                  onClick={() => this.onToggleService('other', id)}
-                  active={services.other[id].selected}
-                >
-                  {services.other[id].name}
-                </Selector.Option>
-              ))}
-              <Selector.Option align="center">+ Add another service</Selector.Option>
-            </Selector>
-          </Accordion.Content>
-        </Accordion>
-
+        <NavBar title={PAGES.INFO} />
+        {page === PAGES.INFO && (
+          <ServicesInfoPage services={services} onSelectService={this.onSelectService} />
+        )}
+        {page === PAGES.RECAP && <ServicesRecapPage services={services} />}
         <div className="position-absolute" style={{ right: 0, bottom: 0, left: 0 }}>
           <Button fluid primary onClick={this.onNext}>
             Next
---
+++ src/app/services/ServicesRecapPage.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+
+import ListItem from './ListItem';
+import Header from '../../components/header';
+import SectionHeader from '../../components/sectionHeader';
+
+class ServicesRecapPage extends Component {
+  getServiceItems = (category) => {
+    const { services } = this.props;
+
+    return Object.keys(services[category]).map((id) => {
+      const service = services[category][id];
+
+      return service.selected ? (
+        <ListItem title={service.name} progress={service.progress || 0} />
+      ) : (
+        undefined
+      );
+    });
+  };
+
+  render() {
+    return (
+      <div className="text-left">
+        <div className="py-5 px-3 container">
+          <Header>
+            Please fill in all the information available for each of the services at this location:
+          </Header>
+        </div>
+
+        <SectionHeader title="Shelter" icon="home" />
+        {this.getServiceItems('shelter')}
+
+        <SectionHeader title="Food" icon="cutlery" />
+        {this.getServiceItems('food')}
+
+        <SectionHeader title="Other Services" icon="ellipsis-h" />
+        {this.getServiceItems('other')}
+      </div>
+    );
+  }
+}
+
+export default ServicesRecapPage;
