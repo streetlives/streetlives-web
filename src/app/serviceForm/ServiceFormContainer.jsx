af2e38bae03315ae4ba7f49f549ede2943ce36f3
nlaz <nikolazaris@utexas.edu>
2018-05-01 23:31:22 -0400

    update service form container

--- src/app/serviceForm/ServiceFormContainer.jsx
+++ src/app/serviceForm/ServiceFormContainer.jsx
@@ -3,18 +3,35 @@ import React, { Component } from 'react';
 import Button from '../../components/button';
 import Icon from '../../components/icon';
 
-import ServiceFormRoutes from './routes';
+import ServiceFormRoutes, { SERVICE_FIELDS as routes } from './routes';
 import NavBar from '../NavBar';
 import ProgressBar from '../locationInfo/ProgressBar';
 
-// eslint-disable-next-line
 class ServiceFormContainer extends Component {
+  onBack = () => {
+    const { locationId, serviceId } = this.props.match.params;
+    const prevRoute = routes[this.getCurrentIndex() - 1];
+    this.props.history.push(`/location/${locationId}/services/${serviceId}${prevRoute.route}`);
+  };
+  onNext = () => {
+    const { locationId, serviceId } = this.props.match.params;
+    const nextRoute = routes[this.getCurrentIndex() + 1];
+    this.props.history.push(`/location/${locationId}/services/${serviceId}${nextRoute.route}`);
+  };
+
+  getCurrentIndex = () => {
+    const { fieldName } = this.props.match.params;
+    return routes.map(({ route }) => route.split('/').pop()).indexOf(fieldName);
+  };
+
   render() {
-    const { onBack, onNext, index } = this.props; // TODO
+    const index = this.getCurrentIndex();
+    const currentRoute = routes[index];
+
     return (
       <div className="text-left">
-        <NavBar title="Service description" />
-        <ProgressBar step={0} steps={10} />
+        <NavBar title={currentRoute.title} />
+        <ProgressBar step={index + 1} steps={routes.length} />
         <div className="container">
           <div className="row px-4">
             <ServiceFormRoutes />
@@ -23,10 +40,10 @@ class ServiceFormContainer extends Component {
         <div className="position-absolute" style={{ right: 0, bottom: 12 }}>
           <div className="container">
             <div className="row px-4">
-              <Button onClick={onBack} compact disabled={index === 0}>
+              <Button onClick={this.onBack} compact disabled={index === 0}>
                 <Icon name="chevron-up" />
               </Button>
-              <Button onClick={onNext} compact>
+              <Button onClick={this.onNext} compact disabled={routes.length - 1 === index}>
                 <Icon name="chevron-down" />
               </Button>
             </div>
