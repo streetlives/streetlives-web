5fa08d02ceedc7e111d3d9e15d27234a783d87a9
Jacob Beard <jake@minnow.io>
2018-05-02 20:43:48 -0400

    Remove erroneous disableAuth config.

--- src/config.js
+++ src/config.js
@@ -3,5 +3,4 @@ export default {
   gaTrackingId: process.env.REACT_APP_GA_TRACKING_ID || 'UA-116475051-1',
   baseApi: process.env.REACT_APP_API_URL || 'http://localhost:3001',
   googleMaps: process.env.REACT_APP_GMAPS_URL || 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCFMDqEgQ6VWWJbROzZhHu-f6sktAmTEGU&v=3.exp&libraries=geometry,drawing,places',
-  disableAuth: true
 };
