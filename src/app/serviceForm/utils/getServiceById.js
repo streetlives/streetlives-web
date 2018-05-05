d5e8df16d56140bfef320d775925d224143a160e
nlaz <nikolazaris@utexas.edu>
2018-05-05 18:44:29 -0400

    remove get service by id util

--- src/app/serviceForm/utils/getServiceById.js
+++
@@ -1,8 +0,0 @@
-export default function getServiceById(location, serviceId) {
-  try {
-    return location.Services.filter(service => service.id === serviceId)[0];
-  } catch (error) {
-    // eslint-disable-next-line no-console
-    return console.error("This service doesn't exist for this location");
-  }
-}
