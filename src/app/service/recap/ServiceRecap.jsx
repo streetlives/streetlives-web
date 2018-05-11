c9fe1c6c4da61a8f5198a0b85c188cd7b3b86e73
nlaz <nikolazaris@utexas.edu>
2018-05-11 12:37:06 -0400

    fix added services on service recap

--- src/app/service/recap/ServiceRecap.jsx
+++ src/app/service/recap/ServiceRecap.jsx
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
 
 import { getLocation } from '../../../selectors/location';
-import { getTaxonomy } from '../../../selectors/taxonomy';
+import { getTaxonomyForLocation } from '../../../selectors/taxonomy';
 import * as actions from '../../../actions';
 import Button from '../../../components/button';
 import Header from '../../../components/header';
@@ -35,7 +35,7 @@ class ServicesRecap extends Component {
     const { locationId } = this.props.match.params;
     this.props.getLocation(locationId);
 
-    if (!this.props.taxonomy) {
+    if (this.props.taxonomy.length === 0) {
       this.props.getTaxonomy();
     }
   }
@@ -46,7 +46,8 @@ class ServicesRecap extends Component {
         nextProps.locationData.Services &&
         nextProps.locationData.Services.map(service => ({
           ...service,
-          parent_id: service.Taxonomies[0] && service.Taxonomies[0].parent_id,
+          parent_id:
+            (service.Taxonomies[0] && service.Taxonomies[0].parent_id) || service.Taxonomies[0].id,
         }));
       this.setState({ services });
     }
@@ -66,7 +67,7 @@ class ServicesRecap extends Component {
     const { taxonomy = [] } = this.props;
     const { services = [] } = this.state;
 
-    if (!taxonomy || !services) {
+    if (taxonomy.length === 0 || services.length === 0) {
       return <LoadingView />;
     }
 
@@ -118,7 +119,7 @@ class ServicesRecap extends Component {
 
 const mapStateToProps = (state, ownProps) => ({
   locationData: getLocation(state, ownProps),
-  taxonomy: getTaxonomy(state, ownProps),
+  taxonomy: getTaxonomyForLocation(state, ownProps),
 });
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
