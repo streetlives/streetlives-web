d84e886d446a938199ee4f9421aff30faea4c69d
rovack <rovack@walla.co.il>
2018-08-05 14:04:10 -0400

    Handle 404 in the sub-router for comments

--- src/app/comments/Router.jsx
+++ src/app/comments/Router.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
+import NotFound from '../notFound/NotFound';
 
 import NewCommentForm from './newCommentForm/NewCommentForm';
 import ViewComments from './viewComments/ViewComments';
 import Intro from './intro/Intro';
 import Thanks from './thanks/Thanks';
 
-// TODO: Handle 404.
-
 export default function Router({ match }) {
   return (
-    <div className="CommentsRouter">
+    <Switch>
       <Route exact path={`${match.path}/:locationId`} component={Intro} />
       <Route exact path={`${match.path}/:locationId/view`} component={ViewComments} />
       <Route exact path={`${match.path}/:locationId/add`} component={NewCommentForm} />
       <Route exact path={`${match.path}/:locationId/thanks`} component={Thanks} />
-    </div>
+      <Route path="*" component={NotFound} />
+    </Switch>
   );
 }
