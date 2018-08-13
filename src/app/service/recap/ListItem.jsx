646597dcd6e5efd64b038c7746541ef299c4f51e
Jacob Beard <jake@minnow.io>
2018-08-13 15:12:38 -0400

    Fixes #35

--- src/app/service/recap/ListItem.jsx
+++ src/app/service/recap/ListItem.jsx
@@ -2,8 +2,21 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import Icon from '../../../components/icon';
+import { SERVICE_FIELDS } from '../../serviceForm/routes';
+import { DOCUMENT_FIELDS } from '../../documents/routes';
 
 import './ListItem.css';
+
+function filterRoutes(service, route){
+  return service.metadata[route.metaDataSection].find( metadata => metadata.field_name === route.fieldName)
+}
+
+function getProgress(service){
+  const totalNumberOfFields = SERVICE_FIELDS.length + DOCUMENT_FIELDS.length;
+  const f = filterRoutes.bind(this, service);
+  const fieldsWithMetadata = SERVICE_FIELDS.filter(f).length + DOCUMENT_FIELDS.filter(f).length;
+  return Math.round((fieldsWithMetadata / totalNumberOfFields) * 100)
+}
 
 function ListItem({ service }) {
   return (
@@ -11,7 +24,7 @@ function ListItem({ service }) {
       <div className="container d-flex justify-content-between align-items-center">
         <div>
           <h5 className="font-weight-normal m-0">{service.name}</h5>
-          <span className="text-secondary">{service.progress || 0}% completed</span>
+          <span className="text-secondary">{getProgress(service)}% completed</span>
         </div>
         <Icon name="chevron-right" size="lg" />
       </div>
