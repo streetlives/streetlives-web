91a8dbb181bb13b0a01004a8c81ed145b75ee504
Jacob Beard <jake@jacobeanrnd.com>
2018-08-18 20:58:29 -0400

    Avoid creating a new anonymous function on render in ErrorLabel

--- src/components/form/ErrorLabel.jsx
+++ src/components/form/ErrorLabel.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import './ErrorLabel.css'
 
+function reload(){
+  window.location.reload();
+}
+
 const ErrorLabel = ({ errorMessage }) => (
   <div className="FormEdit">
     <p>
       <i className="fas fa-exclamation-triangle" aria-hidden="true" />&nbsp;
       <span>{errorMessage}</span>
     </p>
-    <p onClick={() => {window.location.reload()}}> Click here to reload the page </p>
+    <p onClick={reload}> Click here to reload the page </p>
   </div>
 );
 
