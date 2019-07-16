e07e1c1b39dfb45341d14ce360f6e1d7e24c6213
rovack <rovack@walla.co.il>
2019-07-16 17:05:54 -0400

    Fix modal issues on different screen sizes
    - Fix bug with compact modals going beyond the top of the screen.
    - Reduce horizontal padding to reduce risk of icons overlapping
      with the text within buttons in the confirmation modals.

--- src/app/find/questionFlow/QuitFlowConfirmation.jsx
+++ src/app/find/questionFlow/QuitFlowConfirmation.jsx
@@ -5,7 +5,7 @@ import Button from '../../../components/button';
 import Icon from '../../../components/icon';
 
 const QuitFlowConfirmation = ({ onQuit, onClose }) => (
-  <Modal compact className="text-left px-5 py-4">
+  <Modal compact className="text-left px-4 py-4">
     <Icon size="3x" name="exclamation-triangle" className="mt-4 mb-2" />
     <Header size="medium" className="mt-4">
       Are you sure you want to quit your search?
