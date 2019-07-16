242d23960ed8c744257bf23953804f5987387813
rovack <rovack@walla.co.il>
2019-07-16 13:34:44 -0400

    Add the first question ("clothes for")

--- src/app/find/questionFlow/categoryQuestions/clothing/ClothingForQuestion.jsx
+++ src/app/find/questionFlow/categoryQuestions/clothing/ClothingForQuestion.jsx
@@ -1,5 +1,28 @@
 import React from 'react';
+import OptionButtons from '../../../../../components/optionButtons';
 
-// TODO: Implement.
-
-export default () => <div />;
+export default ({ value, onAnswer }) => (
+  <OptionButtons>
+    <OptionButtons.Option
+      iconName="user"
+      active={value === 'adults'}
+      onClick={() => onAnswer('adults')}
+    >
+      Only me
+    </OptionButtons.Option>
+    <OptionButtons.Option
+      iconName="users"
+      active={value === null}
+      onClick={() => onAnswer(null)}
+    >
+      Me and my kid(s)
+    </OptionButtons.Option>
+    <OptionButtons.Option
+      iconName="child"
+      active={value === 'children'}
+      onClick={() => onAnswer('children')}
+    >
+      Only my kid(s)
+    </OptionButtons.Option>
+  </OptionButtons>
+);
