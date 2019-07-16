d6c6dcaa0ceceb4bcc5cf5e545b72c3efc0910ec
rovack <rovack@walla.co.il>
2019-07-16 11:37:54 -0400

    Fix issue where MapPage gets unmounted when going into questions
    - Replace the complicated rendering in the find router with just making
      MapPage path not exact, so it stays mounted when changing paths.
    - Since removing exact matching unfortunately means it'll match any
      path starting with "/find", remove "not found" page.

--- src/app/find/Router.jsx
+++ src/app/find/Router.jsx
@@ -1,35 +1,17 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import NotFound from '../notFound/NotFound';
+import { Route } from 'react-router-dom';
 
 import MapPageContainer from './mapPage/MapPageContainer';
 import QuestionFlowContainer from './questionFlow/QuestionFlowContainer';
 
-const MapPageRouter = ({ baseMatch }) => (
-  <div>
-    <Route component={MapPageContainer} />
-    <Route
-      path={`${baseMatch.path}/:categoryName/questions/:question?`}
-      component={QuestionFlowContainer}
-    />
-  </div>
-);
-
 export default function Router({ match }) {
   return (
-    <Switch>
-      {[
-        `${match.path}/:categoryName?`,
-        `${match.path}/:categoryName/questions/:question?`,
-      ].map(path => (
-        <Route
-          key={path}
-          exact
-          path={path}
-          render={routeProps => <MapPageRouter {...routeProps} baseMatch={match} />}
-        />
-      ))}
-      <Route path="*" component={NotFound} />
-    </Switch>
+    <div>
+      <Route path={`${match.path}/:categoryName?`} component={MapPageContainer} />
+      <Route
+        path={`${match.path}/:categoryName/questions/:question?`}
+        component={QuestionFlowContainer}
+      />
+    </div>
   );
 }
