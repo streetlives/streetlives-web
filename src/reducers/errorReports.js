096166e6b038eb13fed173231a406296ee720d89
cadecameron <cade.cameron@gmail.com>
2020-05-08 13:47:26 -0700

    REMOVED code related to unused error report retrival and removal.

--- src/reducers/errorReports.js
+++ src/reducers/errorReports.js
@@ -1,21 +1,11 @@
 import {
-  GET_ERROR_REPORT_RESPONSE,
   POST_ERROR_REPORT_REQUEST,
   POST_ERROR_REPORT_SUCCESS,
   POST_ERROR_REPORT_ERROR,
-  DELETE_ERROR_REPORT_REQUEST,
-  // DELETE_ERROR_REPORT_SUCCESS, // deletion rolled back if error only
-  DELETE_ERROR_REPORT_ERROR,
 } from '../actions';
 
 export const errorReportsReducer = (state = { isPosting: false }, action) => {
   switch (action.type) {
-    case GET_ERROR_REPORT_RESPONSE:
-      return {
-        ...state,
-        [action.payload.locationId]: action.payload.errorReports,
-      };
-
     case POST_ERROR_REPORT_REQUEST:
       return {
         ...state,
@@ -39,32 +29,6 @@ export const errorReportsReducer = (state = { isPosting: false }, action) => {
       return {
         ...state,
         isPosting: false,
-      };
-
-    case DELETE_ERROR_REPORT_REQUEST: {
-      const { locationId, errorReport } = action.payload;
-      const errorReports = state[locationId] || [];
-      const errorReportIndex = errorReports.indexOf(errorReport);
-
-      if (errorReportIndex === -1) {
-        return state;
-      }
-
-      return {
-        ...state,
-        [`last/${locationId}`]: errorReports,
-        [locationId]: [
-          ...errorReports.slice(0, errorReportIndex),
-          ...errorReports.slice(errorReportIndex + 1),
-        ],
-      };
-    }
-
-    case DELETE_ERROR_REPORT_ERROR:
-      return {
-        ...state,
-        [`last/${action.payload.locationId}`]: null,
-        [action.payload.locationId]: state[`last/${action.payload.locationId}`],
       };
 
     default:
--- src/services/api.js
+++ src/services/api.js
@@ -248,18 +248,6 @@ export const removeComment = comment =>
       },
     }));
 
-/* ------ Error Report API functions -------- */
-export const getErrorReports = ({ locationId }) =>
-  axios
-    .request({
-      url: `${config.baseApi}/errorreports`,
-      method: 'get',
-      params: {
-        locationId,
-      },
-    })
-    .then(result => result.data);
-
 export const postErrorReport = ({ locationId, errorReport }) =>
   getAuthToken() // Remove authorization requirement for posting new error reports?
     .then(idJwtToken => axios.request({
@@ -276,13 +264,3 @@ export const postErrorReport = ({ locationId, errorReport }) =>
       },
     }))
     .then(result => result.data);
-
-export const deleteErrorReport = errorReport =>
-  getAuthToken()
-    .then(idJwtToken => axios.request({
-      url: `${config.baseApi}/errorreports/${errorReport.id}`,
-      method: 'delete',
-      headers: {
-        Authorization: idJwtToken,
-      },
-    }));
