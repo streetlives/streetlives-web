d1eded6acef1a6f59eec15ea9fb5ea7320c709b1
cadecameron <cade.cameron@gmail.com>
2020-05-11 15:17:14 -0700

    UPDATED error report text component to pure functional component.

--- src/app/errorReports/newErrorReportForm/ErrorReportText.jsx
+++ src/app/errorReports/newErrorReportForm/ErrorReportText.jsx
@@ -1,67 +1,62 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Header from '../../../components/header';
 import Button from '../../../components/button';
 import TextArea from '../../../components/textarea';
 import withErrorReportsForm from '../withErrorReportsForm';
 
-class ErrorReportText extends Component {
-  constructor() {
-    super();
-    this.onTextChange = this.onTextChange.bind(this);
-  }
+const ErrorReportText = (props) => {
+  const {
+    errorReportText,
+    onChange,
+    onSubmit,
+  } = props;
 
-  onTextChange(event) {
-    this.props.onChange(event.target.value);
-  }
-
-  render() {
-    return (
-      <div>
-        <Header size="large" className="locationTitle">Error Report</Header>
-        <div className="mx-4 mb-3 text-left">
-          <div className="w-100 mr-5">
-            <Header size="medium" className="mb-3">
-              Please provide the correction below
-              <br />
-              (Please don&#39;t enter any private information)
-            </Header>
-          </div>
-        </div>
-        <div>
-          <div className="mx-4 mb-1">
-            <TextArea
-              value={this.props.errorReportText}
-              onChange={this.onTextChange}
-              minRows={7}
-              autoFocus
-              fluid
-              className="border" // flex-1 py-2 pl-2 pr-2
-            />
-          </div>
-          <div className="mx-4 mb-1">
-            <Button
-              onClick={this.props.onSubmit}
-              primary
-              fluid
-            >
-              Submit
-            </Button>
-          </div>
-          <div className="mx-4">
-            <Button
-              onClick={() => console.log('Cancel button triggered. Still need to implement function.')}
-              secondary
-              fluid
-            >
-              Cancel
-            </Button>
-          </div>
+  return (
+    <div>
+      <Header size="large" className="locationTitle">Error Report</Header>
+      <div className="mx-4 mb-3 text-left">
+        <div className="w-100 mr-5">
+          <Header size="medium" className="mb-3">
+            Please provide the correction below
+            <br />
+            (Please don&#39;t enter any private information)
+          </Header>
         </div>
       </div>
-    );
-  }
-}
+      <div>
+        <div className="mx-4 mb-1">
+          <TextArea
+            value={errorReportText}
+            onChange={onChange}
+            minRows={7}
+            autoFocus
+            fluid
+            className="border"
+          />
+        </div>
+        <div className="mx-4 mb-1">
+          <Button
+            onClick={onSubmit}
+            primary
+            fluid
+          >
+            Done
+          </Button>
+        </div>
+        <div className="mx-4">
+          <Button
+            onClick={() => console.log('Cancel button triggered. Still need to implement function.')}
+            secondary
+            fluid
+          >
+            Cancel
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 ErrorReportText.propTypes = {
   errorReportText: PropTypes.string.isRequired,
