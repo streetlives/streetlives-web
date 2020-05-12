2c6e76d9440a650ade45c4d75467e1e87056c302
cadecameron <cade.cameron@gmail.com>
2020-05-12 13:11:12 -0700

    UPDATED error report tahnks component to pure functional component.

--- src/app/errorReports/Thanks.jsx
+++ src/app/errorReports/Thanks.jsx
@@ -1,47 +1,38 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Button from '../../components/button';
 import Heart from '../../components/heart';
 import withErrorReportsForm from './withErrorReportsForm';
 
-class Thanks extends Component {
-  constructor(props) {
-    super(props);
-    this.goToViewLocation = this.goToViewLocation.bind(this);
-  }
+const Thanks = (props) => {
+  const { goToViewLocation } = props;
 
-  goToViewLocation() {
-    this.props.history.push(`/find/location/${this.props.match.params.locationId}`);
-  }
-
-  render() {
-    return (
-      <div>
-        <div className="content text-left">
-          <p>
-            <Heart width="100" height="100" />
-          </p>
-          <p className="Header">Thank you so much!</p>
-          <p>
-            You&#39;re helping everyone to get more reliable information.
-            And the reliable information is making it easier for people to get the help they need.
-          </p>
-          <p>
-            Thank you.
-          </p>
-        </div>
-        <div className="mx-4">
-          <Button
-            className="mx-4"
-            primary
-            fluid
-            onClick={this.goToViewLocation}
-          >
-            Done
-          </Button>
-        </div>
+  return (
+    <div>
+      <div className="content text-left">
+        <p>
+          <Heart width="100" height="100" />
+        </p>
+        <p className="Header">Thank you so much!</p>
+        <p>
+          You&#39;re helping everyone to get more reliable information.
+          And the reliable information is making it easier for people to get the help they need.
+        </p>
+        <p>
+          Thank you.
+        </p>
       </div>
-    );
-  }
-}
+      <div className="mx-4">
+        <Button
+          className="mx-4"
+          primary
+          fluid
+          onClick={goToViewLocation}
+        >
+          Done
+        </Button>
+      </div>
+    </div>
+  );
+};
 
 export default withErrorReportsForm(Thanks);
