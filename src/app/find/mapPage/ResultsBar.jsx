aa3bc226f5db89051dd507cef66b4205c0da8194
Weronika <weronika@Adams-MacBook-Pro-2.local>
2020-05-16 15:06:28 -0400

    feat: results bar doesn't update on map drag

--- src/app/find/mapPage/ResultsBar.jsx
+++ src/app/find/mapPage/ResultsBar.jsx
@@ -1,33 +1,41 @@
-import React from "react";
-import Icon from "../../../components/icon";
+import React from 'react';
+import Icon from '../../../components/icon';
 
-function ResultsBar({
-  isSearching,
-  filterString,
-  hasResults,
-  filters,
-  clearResults,
-}) {
-  const noResultsInfo = () => {
+class ResultsBar extends React.Component {
+  shouldComponentUpdate(nextProps) {
+    // this prevents the results bar from updating on map drag
+    if (
+      this.props.filterString === nextProps.filterString &&
+      this.props.isSearching === nextProps.isSearching
+    ) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  noResultsInfo = () => {
+    const { hasResults, filters, clearResults } = this.props;
     const searchTerm = filters.searchString;
+
     // No results after searching through the searchbar
     if (searchTerm && searchTerm.length && !hasResults) {
-      const wordCount = searchTerm.split(" ").length;
+      const wordCount = searchTerm.split(' ').length;
       return (
-        <div className="resultsBarInner">
-          {wordCount > 3 ? (
+        <div className='resultsBarInner'>
+          {wordCount >= 3 ? (
             <div>
-              Nothing matched for "{searchTerm}", try using{" "}
-              <span className="font-weight-bold">fewer search words</span>.
+              Nothing matched for '{searchTerm.trim()}', try using{' '}
+              <span className='font-weight-bold'>fewer search words</span>.
             </div>
           ) : (
             <div>
-              Nothing matched for "{searchTerm}".
-              <span className="font-weight-bold"> Try another word.</span>
+              Nothing matched for '{searchTerm.trim()}'.
+              <span className='font-weight-bold'> Try another word.</span>
             </div>
           )}
-          <div onClick={clearResults} className="iconWrapper">
-            <Icon name="times"/>
+          <div onClick={clearResults} className='iconWrapper'>
+            <Icon name='times' />
           </div>
         </div>
       );
@@ -36,32 +44,33 @@ function ResultsBar({
       Object.keys(filters.advancedFilters) &&
       !hasResults
     ) {
-      // user applied filters after selecting one of the four catagories
-      // and we returned no results.
+      // Handle no results after using filters
       return (
         <div>
-          Nothing matched, try using{" "}
-          <span className="font-weight-bold">different filters.</span>
+          Nothing matched, try using{' '}
+          <span className='font-weight-bold'>different filters.</span>
         </div>
       );
     } else {
       // All other cases
       // For example, user gets no results after going through clothing eligibility flow.
       return (
-        <div className="resultsBarInner">
+        <div className='resultsBarInner'>
           <div>
-            Nothing matched. Try a{" "}
-            <span className="font-weight-bold">more general search.</span>
+            Nothing matched. Try a{' '}
+            <span className='font-weight-bold'>more general search.</span>
           </div>
-          <div onClick={clearResults} className="iconWrapper">
-            <Icon name="times"/>
+          <div onClick={clearResults} className='iconWrapper'>
+            <Icon name='times' />
           </div>
         </div>
       );
     }
   };
 
-  const displayContent = () => {
+  displayContent = () => {
+    const { isSearching, hasResults, filterString } = this.props;
+
     if (isSearching) {
       // loading state
       return <div>Loading results...</div>;
@@ -70,17 +79,18 @@ function ResultsBar({
       return (
         <div>
           Showing results for
-          <span className="font-weight-bold ml-1">{filterString}</span>
+          <span className='font-weight-bold ml-1'>{filterString}</span>
         </div>
       );
     } else {
-      //different handling of no results
-
-      return noResultsInfo();
+      // handle no results
+      return this.noResultsInfo();
     }
   };
 
-  return <div className="resultsBar">{displayContent()}</div>;
+  render() {
+    return <div className='resultsBar'>{this.displayContent()}</div>;
+  }
 }
 
 export default ResultsBar;
