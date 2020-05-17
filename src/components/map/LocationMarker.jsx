d68287f00e0ad352939d46390157ba97cf5b5458
Adam Nguyen <adamngu@gmail.com>
2020-05-17 18:41:35 -0400

    feature/blue-icon-if-active-gray-if-not map view

--- src/components/map/LocationMarker.jsx
+++ src/components/map/LocationMarker.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import { Marker, InfoWindow } from 'react-google-maps';
 import Button from '../button';
 
+const MARKER_ICON_MAPPINGS = {
+  blue: 'https://www.google.com/maps/vt/icon/name=assets/icons/spotlight/spotlight_pin_v2_shadow-1-small.png,assets/icons/spotlight/spotlight_pin_v2-1-small.png,assets/icons/spotlight/spotlight_pin_v2_dot-1-small.png,assets/icons/spotlight/spotlight_pin_v2_accent-1-small.png&highlight=000000,1400FF,FFFFFF,000000&color=1400FF?scale=1',
+  gray: 'https://www.google.com/maps/vt/icon/name=assets/icons/spotlight/spotlight_pin_v2_shadow-1-small.png,assets/icons/spotlight/spotlight_pin_v2-1-small.png,assets/icons/spotlight/spotlight_pin_v2_dot-1-small.png,assets/icons/spotlight/spotlight_pin_v2_accent-1-small.png&highlight=000000,C0C0C0,FFFFFF,000000&color=C0C0C0?scale=1',
+};
+
 function LocationMarker(props) {
   const {
     id,
@@ -11,6 +16,7 @@ function LocationMarker(props) {
     onClose,
     onSubmit,
     children,
+    color = 'blue',
   } = props;
   const position = {
     lng: mapLocation.position.coordinates[0],
@@ -18,7 +24,7 @@ function LocationMarker(props) {
   };
 
   return (
-    <Marker key={id} position={position} onClick={onClick}>
+    <Marker key={id} position={position} onClick={onClick} icon={{ url: MARKER_ICON_MAPPINGS[color] }}>
       {isOpen && (
         <InfoWindow
           options={{
