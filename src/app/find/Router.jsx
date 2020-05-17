f4612cdf5f13c8be2008c887589c63ee5001972e
cadecameron <cade.cameron@gmail.com>
2020-05-17 16:06:06 -0700

    UPDATED find router to include error report paths.

--- src/app/find/Router.jsx
+++ src/app/find/Router.jsx
@@ -9,20 +9,31 @@ import ErrorReportContainer from './errorReports/ErrorReportContainer';
 export default function Router({ match }) {
   return (
     <div>
-      <Route path={`${match.path}/:categoryName?`} component={MapPageContainer} />
+      <Route
+        path={`${match.path}/:categoryName?`}
+        component={MapPageContainer}
+      />
       <Route
         path={`${match.path}/:categoryName/questions/:question?`}
         component={QuestionFlowContainer}
       />
-
-      <Route path={`${match.path}/location/:locationId`} component={LocationDetailsContainer} />
+      <Route
+        exact
+        path={`${match.path}/location/:locationId`}
+        component={LocationDetailsContainer}
+      />
       <Route
         path={`${match.path}/location/:locationId/errorreports`}
         component={ErrorReportContainer}
       />
       <Route
+        exact
         path={`${match.path}/:categoryName/location/:locationId`}
         component={LocationDetailsContainer}
+      />
+      <Route
+        path={`${match.path}/:categoryName/location/:locationId/errorreports`}
+        component={ErrorReportContainer}
       />
     </div>
   );
