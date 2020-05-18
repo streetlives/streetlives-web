8f6d25680fb74d88425207041a1a141bf21ab0b5
Weronika Tomaszewska <veratomaszewska@gmail.com>
2020-05-17 23:11:38 -0400

    resolve spacing issue

--- src/app/landing/CovidInfoModal.jsx
+++ src/app/landing/CovidInfoModal.jsx
@@ -34,7 +34,7 @@ function CovidInfoModal({ history, closeModal }) {
           </div>
           <div className="text-center legal">
             <a href="https://docs.google.com/document/d/e/2PACX-1vTBNI2Sv5QC8DSBwBL7WHNBdMI-9kPLuN2ev_Y2VDSo-bLeh8qssi7iBv-w0EEQurX9fgFQF4_2lItn/pub" target="_blank" rel="noopener noreferrer">Terms of Use</a>
-            and
+            {' '}and{' '}
             <a href="https://docs.google.com/document/d/e/2PACX-1vQ8djXWiASXKkcKJ2LSHNXMgrQ1PhQPskSs_Thk5oLTZMnKvMy7Nqz1t4Xs18mGGKuORXOj8yNNhJUq/pub" target="_blank" rel="noopener noreferrer">Privacy Policy</a>
           </div>
         </div>
