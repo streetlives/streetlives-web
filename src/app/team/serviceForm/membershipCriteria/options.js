3f56b8cbb27e0faf752c0b04a17f6815de16e81a
Abimael Martell <abimex@gmail.com>
2020-05-24 21:47:27 -0700

    Add membership required field for services

---
+++ src/app/team/serviceForm/membershipCriteria/options.js
@@ -0,0 +1,31 @@
+const options = [
+  {
+    label: 'None',
+    values: [false],
+  },
+  {
+    label: 'Referal Letter',
+    requiredDocument: 'referral letter',
+    values: [true, false],
+  },
+  {
+    label: 'Client/member only',
+    values: [true],
+  },
+];
+
+const getLabelByValues = (values) => {
+  const selected = options.find(o => values.toString() === o.values.toString());
+  return selected && selected.label;
+};
+
+const getRequiredDocument = (values) => {
+  const selected = options.find(o => values.toString() === o.values.toString());
+  return selected && selected.requiredDocument;
+};
+
+export {
+  options as default,
+  getLabelByValues,
+  getRequiredDocument,
+};
