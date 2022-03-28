275731c9b176dfbc9838afd4dcc8a2471df0ac5a
Tom Tytunovich <tom.tytunovich@gmail.com>
2022-03-29 02:18:56 +0300

    Revert "Remove the deprecated babel-polyfill"
    This reverts commit 7cbd615ee0a5a7d1797fada3357688a3e4c53945.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 /* eslint-disable react/jsx-filename-extension */
+import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
