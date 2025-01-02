d18ac0ce51be8a081d7b52e9818ffc6431b0f825
Sk <shakil@meghops.com>
2025-01-02 14:00:13 +0600

    fix document required

--- src/selectors/document.js
+++ src/selectors/document.js
@@ -2,8 +2,10 @@ import { getService } from './service';
 
 export const getDocuments = (state, props) => getService(state, props).DocumentsInfo || {};
 
-export const getRequiredDocuments = (state, props) =>
-  (getService(state, props).RequiredDocuments || []).map(doc => doc.document);
+export const getRequiredDocuments = (state, props) => {
+  const docs = (getService(state, props).RequiredDocuments || []).map(doc => doc.document);
+  return docs.length ? docs : [];
+};
 
 export const getDocumentRecertificationTime = (state, props) =>
   getDocuments(state, props).recertification_time;
