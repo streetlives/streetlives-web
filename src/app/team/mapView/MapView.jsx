a20a344791fc6b15ee18040f523b249a21753179
shakil hossain <sh9743441@gmail.com>
2026-06-03 20:54:19 +0600

    fix: update marker color logic for closed locations

--- src/app/team/mapView/MapView.jsx
+++ src/app/team/mapView/MapView.jsx
@@ -139,7 +139,7 @@ export default class MapView extends Component {
           <ExistingLocationMarker
             key={location.id}
             mapLocation={location}
-            color={(location.Services && location.Services.length) || (location.services && location.services.length) ? 'blue' : 'red'}
+            color={(location.Services && location.Services.length) || (location.services && location.services.length) ? location.closed ? 'gray' :  'blue' : 'red'}
             isOpen={location.id === this.state.openLocationId}
             onToggleInfo={this.onToggleMarkerInfo}
             onEnterLocation={this.editLocation}
